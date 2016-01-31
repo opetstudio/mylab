187cbfe8d9de69338b333de0f3d32a3899f562ad
nofrets.poai <nofrets.poai@solusi247.com>
2016-01-31 20:12:45 +0700

    get multiple 3 and 5 using for loop

--- Task1.php
+++ Task1.php
@@ -4,6 +4,11 @@ class Task1
 	public function fizzBuzz($from=0,$to=0){
 		if($from < 0 && $to < 0)
 		  throw new Exception("Number must gt 0!");
+		for($i = $from;$i<=$to;$i++){
+			if($i%3==0) echo " Fizz ";
+			elseif($i%5==0) echo " Buzz ";
+			else echo " $i " ;
+		}
 	}
 }
 ?>
