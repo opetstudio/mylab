beed6a4b608e5c8579a2263a07412fa886ad2ad9
nofrets.poai <nofrets.poai@solusi247.com>
2016-01-31 16:28:49 +0700

    edit bug

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-<<<<<<< HEAD
 	echo "Hello World.";
 	echo "Apa kabar?";
 	echo "commit for iss53";
-	echo "dit fiture";
-	
+	echo "Edit fiture";
 ?>
-=======
-	echo "Hello World....";
-	echo "Apa kabar?";
-?>
->>>>>>> 02a0aceff4f6d6e136b7814fc57e6ec7299ee470
