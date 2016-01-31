0fafaa736a72dfdebced4de28bf7ef8984a0f717
nofrets.poai <nofrets.poai@solusi247.com>
2016-01-31 21:54:12 +0700

    suggestions/corrections 5.

--- getinfo.php
+++ getinfo.php
@@ -4,7 +4,7 @@ require_once("data.php");
 $id = $_GET['id'];
 $data = new baseObj();
 
-if (is_object($data) = true) $status = '200 OK';
+if (is_object($data)) $status = '200 OK';
 $status_header = 'HTTP/1.1 $status';
 
 header($status_header);
