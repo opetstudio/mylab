aa787b74740c0a219bea3fdd9620643fadfc32df
nofrets.poai <nofrets.poai@solusi247.com>
2016-02-01 11:55:05 +0700

    create function testFizzBuzzBazz for testing Task 1 part 2

--- Task1Test.php
+++ Task1Test.php
@@ -6,9 +6,13 @@ class Task1Test extends PHPUnit_Framework_TestCase
 {
 	public function testFizzBuzz()
   {
-    // test to ensure that the object from an fsockopen is valid
     $task1 = new Task1();
 	$this->assertTrue($task1->fizzBuzz(1,16));
+  }
+  public function testFizzBuzzBazz()
+  {
+    $task1 = new Task1();
+	$this->assertTrue($task1->fizzBuzzBazz(4,11));
   }
 }
 
