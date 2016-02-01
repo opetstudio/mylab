8af80df006680280f91ced690e39a052b2488531
nofrets.poai <nofrets.poai@solusi247.com>
2016-02-01 11:23:58 +0700

    suggestions/corrections 11. get info using second way

--- getinfo.php
+++ getinfo.php
@@ -8,8 +8,8 @@ if (is_object($data)) $status = '200 OK';
 $status_header = 'HTTP/1.1 '.$status;
 
 header($status_header);
-echo json_encode( $data->getAll($id) );
-echo json_encode( $data->getSwimmingPool($id)->getSwimmingPoolByPID() );
-echo json_encode( $data->getHdbblock($id)->getHdbblockByPID() );
-
+// echo json_encode( $data->getAll($id) );
+// echo json_encode( $data->getSwimmingPool($id)->getSwimmingPoolByPID() );
+// echo json_encode( $data->getHdbblock($id)->getHdbblockByPID() );
+echo json_encode( array_merge($data->getAll($id), $data->getSwimmingPool($id)->getSwimmingPoolByPID(), $data->getHdbblock($id)->getHdbblockByPID()));
 ?>
